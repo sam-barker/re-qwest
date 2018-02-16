13dc58bdb03e890169dd26629e239bfa324d1ead
Samuel Barker <spbarker1994@gmail.com>
2018-02-16 13:13:20 +0000

    Major fix

--- dist/index.js
+++ dist/index.js
@@ -1,74 +1,4 @@
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, {
-/******/ 				configurable: false,
-/******/ 				enumerable: true,
-/******/ 				get: getter
-/******/ 			});
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
-/******/ })
-/************************************************************************/
-/******/ ([
-/* 0 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -76,7 +6,43 @@ Object.defineProperty(exports, "__esModule", {
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-exports.default = qwest;
+exports.default = function (_ref) {
+  var method = _ref.method,
+      url = _ref.url,
+      params = _ref.params,
+      headers = _ref.headers,
+      props = _ref.props;
+
+  return new Promise(function (resolve, reject) {
+    var xhr = getXHR(method, url);
+    if (xhr === null) throw new Error('CORS not supported');
+    xhr.onerror = function () {
+      reject(createError(xhr));
+    };
+    xhr.onload = function () {
+      if (statusOK(xhr)) resolve(xhr.response);else reject(createError(xhr));
+    };
+
+    if (headers) {
+      Object.keys(headers).forEach(function (key) {
+        xhr.setRequestHeader(key, headers[key]);
+      });
+    }
+
+    if (props) {
+      Object.keys(props).forEach(function (key) {
+        Object.defineProperty(xhr, key, {
+          value: props[key],
+          writable: true
+        });
+      });
+    }
+
+    var xhrParams = getXHRParams(params);
+    xhr.send(xhrParams);
+  });
+};
+
 var getXHR = function getXHR(method, url) {
   var xhr = new XMLHttpRequest();
   if ('withCredentials' in xhr) {
@@ -108,37 +74,3 @@ var getXHRParams = function getXHRParams(params) {
     return null;
   }
 };
-
-function qwest(method, url, params, headers, props) {
-  return new Promise(function (resolve, reject) {
-    var xhr = getXHR(method, url);
-    if (xhr === null) throw new Error('CORS not supported');
-    xhr.onerror = function () {
-      reject(createError(xhr));
-    };
-    xhr.onload = function () {
-      if (statusOK(xhr)) resolve(xhr.response);else reject(createError(xhr));
-    };
-
-    if (headers) {
-      Object.keys(headers).forEach(function (key) {
-        xhr.setRequestHeader(key, headers[key]);
-      });
-    }
-
-    if (props) {
-      Object.keys(props).forEach(function (key) {
-        Object.defineProperty(xhr, key, {
-          value: props[key],
-          writable: true
-        });
-      });
-    }
-
-    var xhrParams = getXHRParams(params);
-    xhr.send(xhrParams);
-  });
-}
-
-/***/ })
-/******/ ]);
